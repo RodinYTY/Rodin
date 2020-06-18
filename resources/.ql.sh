83602e673daa01fd818a452985c0e4478ed3494d
RodinYTY <1418583869@qq.com>
2020-06-18 10:09:15 +0800

    .zshrc

--- resources/.ql.sh
+++ resources/.ql.sh
@@ -1,5 +1,5 @@
 brew cask install qlmarkdown			# markdown
 brew cask install qlcolorcode			# 代码高亮
-brew cask install qlvideo				# 视频预览
+brew cask install qlvideo			# 视频预览
 brew cask install quicklook-json		# json
-brew cask install suspicious-package	# pkg
+brew cask install suspicious-package		# pkg
